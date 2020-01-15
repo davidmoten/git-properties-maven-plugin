8c14295a5b06674d85dff0d84325ef475674ce51
David Moten <davidmoten@gmail.com>
2020-01-16 09:58:59 +1100

    merge error stream with stdout

--- src/main/java/com/github/davidmoten/gitp/PropertiesMojo.java
+++ src/main/java/com/github/davidmoten/gitp/PropertiesMojo.java
@@ -6,9 +6,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
@@ -22,33 +25,40 @@ public class PropertiesMojo extends AbstractMojo {
     /**
      * Location of the file.
      */
-    @Parameter(defaultValue = "${project.build.directory}", property = "outputDir", required = true)
+    @Parameter(defaultValue = "${project.build.outputDirectory}", property = "outputDir", required = true)
     private File outputDirectory;
 
     @Parameter(defaultValue = "git.properties", property = "filename", required = true)
     private String filename;
 
     public void execute() throws MojoExecutionException {
-
+        Log log = getLog();
         if (!outputDirectory.exists()) {
             outputDirectory.mkdirs();
         }
 
         File file = new File(outputDirectory, filename);
         try {
-            String commitHash = run("git rev-parse HEAD");
-            String commitTime = run("git show -s --format=%ci HEAD");
+            String commitHash = run("git", "rev-parse", "HEAD");
+            String commitTime = run("git", "show", "-s", "--format=%ci", "HEAD");
             try (FileWriter w = new FileWriter(file)) {
                 w.write("git.commit.hash=" + commitHash.trim() + "\n");
                 w.write("git.commit.timestamp=" + commitTime.trim());
             }
-        } catch (IOException e) {
+            log.info("git.commit.hash=" + commitHash);
+            log.info("git.commit.timestamp=" + commitTime);
+            log.info("git properties written to " + file);
+        } catch (IOException | InterruptedException e) {
             throw new MojoExecutionException(e.getMessage(), e);
         }
     }
 
-    private static String run(String command) throws IOException {
-        Process p = Runtime.getRuntime().exec(command);
+    private static String run(String... command) throws IOException, InterruptedException {
+        Process p = new ProcessBuilder() //
+                .command(Arrays.asList(command)) //
+                .redirectErrorStream(true) //
+                .start();
+        p.waitFor(10, TimeUnit.SECONDS);
         try (InputStream in = p.getInputStream()) {
             byte[] bytes = read(in);
             return new String(bytes, StandardCharsets.UTF_8);
